bdd0d6c77d26ab8bd275c32adcb9e1fbe6de43d9
AL-FAKHRI-ABDUL-NASHIR <nasirrhisk80@gmail.com>
2026-09-19 15:51:23 +0800

    Merubah nama file

---
+++ posttest/posttest-apd-2/2609106009-AL FAKHRI ABDUL NASHIR-PT-2.py
@@ -0,0 +1,33 @@
+barang_1 = 15000
+barang_2 = 25000
+barang_3 = 10000
+barang_4 = 5500
+barang_5 = 5000
+barang_6 = 20000
+
+belanjaan = [barang_1, barang_2, barang_3, barang_4, barang_5, barang_6]
+
+pajak = 0.15 * (barang_1 + barang_2 + barang_3 + barang_4 + barang_5 + barang_6)
+total_bayar = pajak + barang_1 + barang_2 + barang_3 + barang_4 + barang_5 + barang_6
+rata_rata = total_bayar / len(belanjaan)
+
+nim = 9
+bolean = nim < rata_rata
+
+total_bayar_dollar_amerika = 0.000056 * total_bayar
+total_bayar_yuan = 0.00038 * total_bayar
+
+print(barang_1)
+print(barang_2)
+print(barang_3)
+print(barang_4)
+print(barang_5)
+print(barang_6)
+print(belanjaan)
+print(pajak)
+print(total_bayar)
+print(rata_rata)
+print(nim)
+print(bolean)
+print(total_bayar_dollar_amerika)
+print(total_bayar_yuan)
