68b743ef35e10c99a5d88c2c8abd159b4d102eea
AL-FAKHRI-ABDUL-NASHIR <nasirrhisk80@gmail.com>
2026-09-19 20:52:24 +0800

    Merubah format output dan memperjelas variabel mata uang

--- posttest/posttest-apd-2/2609106009-AL FAKHRI ABDUL NASHIR-PT-2.py
+++ posttest/posttest-apd-2/2609106009-AL FAKHRI ABDUL NASHIR-PT-2.py
@@ -14,20 +14,21 @@ rata_rata = total_bayar / len(belanjaan)
 nim = 9
 bolean = nim < rata_rata
 
-total_bayar_dollar_amerika = 0.000056 * total_bayar
-total_bayar_yuan = 0.00038 * total_bayar
+total_bayar_dolar_amerika_serikat = 0.000056 * total_bayar
+total_bayar_yuan_tiongkok = 0.00038 * total_bayar
 
-print(barang_1)
-print(barang_2)
-print(barang_3)
-print(barang_4)
-print(barang_5)
-print(barang_6)
-print(belanjaan)
-print(pajak)
-print(total_bayar)
-print(rata_rata)
-print(nim)
-print(bolean)
-print(total_bayar_dollar_amerika)
-print(total_bayar_yuan)
+print("Harga barang 1 =", barang_1)
+print("Harga barang 2 =", barang_2)
+print("Harga barang 3 =", barang_3)
+print("Harga barang 4 =", barang_4)
+print("Harga barang 5 =", barang_5)
+print("Harga barang 6 =", barang_6)
+print("List semua harga barang secara berurutan =", belanjaan)
+print("Total pajak =", pajak)
+print("Total pembayaran =", total_bayar)
+print("Rata-rata pembayaran =", rata_rata)
+print("Tiga digit terakhir nim =", nim)
+print("Bolean antara nim dan rata-rata =", bolean)
+print("Pembayaran dalam mata uang Dolar Amerika Serikat =", total_bayar_dolar_amerika_serikat, "USD")
+print("Pembayaran dalam mata uang Yuan Tiongkok", total_bayar_yuan_tiongkok, "CNY")
+print("Tampilan harga barang 1, barang 3, dan barang 5 secara berurutan =", belanjaan[0], belanjaan[2], belanjaan[4])
